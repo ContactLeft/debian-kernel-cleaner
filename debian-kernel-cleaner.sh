c6294dda42f370244442e9047a61e75c65f8c688
Denis Khabarov <admin@saymon21-root.pro>
2016-10-13 21:28:43 +0300

    first commit

---
+++ debian-kernel-cleaner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ ! -f /etc/debian_version ]; then
+  echo "Ошибка! Что-то не похоже, что скрипт запущен под deb-based системой."
+  exit 1
+fi
+
+CURRENTKERN=`uname -r`
+NEWESTKERN=`dpkg -l | grep -e linux-image-[0-9] | sed -r 's/ +/ /g' | cut -d' ' -f2 | sed s/linux-image-// | tail -1`
+
+CURRENTKERN_VERSION=`echo $CURRENTKERN | sed -r 's/-[^0-9]+$//'`
+NEWESTKERN_VERSION=`echo $NEWESTKERN | sed -r 's/-[^0-9]+$//'`
+
+dpkg -l 'linux-*' | grep -e linux-image-[0-9] -e linux-headers-[0-9] | grep ^ii | grep -v $CURRENTKERN_VERSION | grep -v $NEWESTKERN_VERSION | sed -r 's/ +/ /g' | cut -d' ' -f2 | xargs apt-get -y purge
+
