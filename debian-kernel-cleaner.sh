209bd80d9c38b27f50c7157639ca45a449d72d6c
Contact_Left <ContactLeft@users.noreply.github.com>
2020-08-15 15:55:55 +0100

    Update debian-kernel-cleaner.sh

--- debian-kernel-cleaner.sh
+++ debian-kernel-cleaner.sh
@@ -12,5 +12,3 @@ CURRENTKERN_VERSION=`echo $CURRENTKERN | sed -r 's/-[^0-9]+$//'`
 NEWESTKERN_VERSION=`echo $NEWESTKERN | sed -r 's/-[^0-9]+$//'`
 
 dpkg -l 'linux-*' | grep -e linux-image-[0-9] -e linux-headers-[0-9] | grep ^ii | grep -v $CURRENTKERN_VERSION | grep -v $NEWESTKERN_VERSION | sed -r 's/ +/ /g' | cut -d' ' -f2 | xargs apt-get -y purge 
-apt-get autoremove -y && update-grub2
-
